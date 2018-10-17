7e2321cecad1e8015d1c71f4aabfea9f7bb33c57
derks.patrick <derks.patrick@lcs-landwehr.de>
2018-10-17 16:07:45 +0200

    add method for removing errors in screeps
    this might not be the best practice but it works for now. I don't now
    what these imports do, but if transcript add something to the imports
    it will not be replaced. So I know, that something is new and need to
    implement that manuelly.

--- build.py
+++ build.py
@@ -316,6 +316,28 @@ def install_env(config):
                                 .format(ret, "' '".join(install_args), config.base_dir))
 
 
+def remove_syntax_problems(config):
+    to_replace = []
+    to_replace.append({"import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';": ''})
+    to_replace.append({"var __name__ = '__main__';": ''})
+    to_replace.append({"module.exports.loop = main;": ''})
+    to_replace.append({"export var main": 'module.exports.loop'})
+
+
+    dist_dir = os.path.join(config.base_dir, 'dist')
+
+    with open(dist_dir + '/main.js') as f:
+        main_js = f.read()    
+    
+    for re in to_replace:
+        for key in re:
+            main_js = main_js.replace(key, re[key])
+
+    with open(dist_dir + '/main.js', 'w') as f:
+        f.write(main_js)
+
+    
+    
 def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     config = load_config(base_dir)
